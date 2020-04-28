497d4c82ee1c3bd5f96ea9aa0bcd281a71235c52
Mairon <mairongallas@gmail.com>
2020-04-28 15:39:21 -0300

    Criado atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,17 +1,22 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=35):
+    def __init__(self, *filhos, nome=None, idade=35):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa('Luciano')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Mairon'
-    print(p.nome)
-    print(p.idade)
+    renzo = Pessoa(nome='Renzo')
+    mairon = Pessoa(renzo, nome='Mairon')
+    print(Pessoa.cumprimentar(mairon))
+    print(id(mairon))
+    print(mairon.cumprimentar())
+    print(mairon.nome)
+    print(mairon.idade)
+    print(mairon.filhos)
+    for filho in mairon.filhos:
+        print(filho.nome)
+
+
